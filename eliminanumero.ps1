3befccb6cd0e5ca33a9f7e1a0fb744b9a1f4441a
fabriziozandonella <fabrizio.zandonella@gmail.com>
2026-05-23 02:49:11 +0200

    Replaced PolyLang with Gtranslate

--- eliminanumero.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem -Path . -Filter *.html -Recurse -File | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw
-    $clean = $content -replace '(?s)<p>[0-9a-fA-F]+\s*</p>', ''
-    Set-Content $_.FullName $clean -NoNewline
-}
